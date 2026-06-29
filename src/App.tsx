cba30dff9c26a2b6358ea67ca6868ffe584fe3c5
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-06-29 17:35:40 +0530

    feat: integrate InputForm and set as default view

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Dummy_Decision } from "./data/dummyData"
 import { calculateResults } from "./data/scoring"
 import { useState } from "react"
 import ResultsView from "./components/ResultsView"
+import InputForm from "./components/InputForm"
 
 
 function App() {
 
-  const [appState] = useState("results")
+  const [appState, setAppState] = useState("input")
   
   return (
     <div>
       <h1>Decision Prioritiser</h1>
+      {appState === "input" && <InputForm />}
       {appState === "results" && <ResultsView result={calculateResults(Dummy_Decision)} />}
     </div>
   )
